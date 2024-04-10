c99650db91ea578c38f3604588a2cdd31e4f8ee6
architg-op <archit.g1998@gmail.com>
2024-04-10 19:28:45 +0530

    Contact form can now be submitted

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
-const sections = document.querySelectorAll(".section");
-const secBtns = document.querySelectorAll(".controls");
-const secBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
 
 function pageTransitions() {
   // button click active class
   for (let i = 0; i < secBtn.length; i++) {
-    secBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
+    secBtn[i].addEventListener('click', function () {
+      let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
+        'active-btn',
+        ''
       );
-      this.className += " active-btn";
+      this.className += ' active-btn';
     });
   }
 
   //Sections Active
-  allSections.addEventListener("click", (e) => {
+  allSections.addEventListener('click', (e) => {
     const id = e.target.dataset.id;
     if (id) {
       secBtns.forEach((btn) => {
-        btn.classList.remove("active");
+        btn.classList.remove('active');
       });
-      e.target.classList.add("active");
+      e.target.classList.add('active');
 
       //hide other sections
       sections.forEach((section) => {
-        section.classList.remove("active");
+        section.classList.remove('active');
       });
 
       const element = document.getElementById(id);
-      element.classList.add("active");
+      element.classList.add('active');
     }
   });
 
   //Toggle Theme
-  const themeBtn = document.querySelector(".theme-btn");
-  themeBtn.addEventListener("click", () => {
+  const themeBtn = document.querySelector('.theme-btn');
+  themeBtn.addEventListener('click', () => {
     let element = document.body;
-    element.classList.toggle("light-mode");
+    element.classList.toggle('light-mode');
   });
-}
-
-function submitBtn() {
-  alert(
-    "Submitting information is not supported at this moment\n\nDrop an Email/Text/Whatsapp to the above-mentioned aliases of Archit (Recommended)"
-  );
 }
 
 pageTransitions();
